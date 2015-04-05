2b9eb478d7b855b4f21107732ea94f8f0e9f4673
RStijn <raeymaekers.stijn@gmail.com>
2015-04-05 23:53:52 +0200

    Added example of conditional flight paths

--- npc_advanced_flightmaster.lua
+++ npc_advanced_flightmaster.lua
@@ -1,6 +1,7 @@
 -- Name: 	Custom Flight Master Script for cMangos. 
 -- Details:	With option to require gold or custom item as price for taxi service & configurable conditions. 
 -- 			Easy to configure. Copy a the Flight[#] {} and enter the correct config for each flight path
+--			You can add Player conditions to your flight paths. See for reference: http://eluna.emudevs.com/Player/index.html
 -- Website: https://github.com/RStijn
 
 -- Init
@@ -12,31 +13,49 @@ local NPC_FLIGHTMASTER = 500022 	-- Creature ID. CreatureType must be 3 & NpcFla
 local MOUNT_DISPLAYID_A = 28135		-- Display ID of flying mount for Alliance
 local MOUNT_DISPLAYID_H = 28135		-- Display ID of flying mount for Horde
 
--- Flight 1
-FLIGHT[1] = {} 
-FLIGHT[1][0] = "Take me up that mountain."		-- NPC Gossip Menu Text
-FLIGHT[1][1] = {	
-	{0, -13838, 3098, 5}, 			-- WAYPOINT 1
-	{0, -13947, 3106, 180},			-- WAYPOINT 2
-	{0, -14099, 3111, 188},
-}
-FLIGHT[1][2] = 2500					-- Cost for taxi services 10000 = 1 Gold	default: 0
-FLIGHT[1][3] = 500000				-- Custom item ID as cost price				default: 0
-FLIGHT[1][4] = 50					-- Amount of custom item to take			default: 0
--- end Flight 1
+local function flightConfig(player)
+	-- Flight 1 - Basic
+	FLIGHT[1] = {} 
+	FLIGHT[1][0] = "Take me up that mountain."		-- NPC Gossip Menu Text
+	FLIGHT[1][1] = {	
+		{0, -13838, 3098, 5}, 			-- WAYPOINT 1
+		{0, -13947, 3106, 180},			-- WAYPOINT 2
+		{0, -14099, 3111, 188},
+	}
+	FLIGHT[1][2] = 2500					-- Cost for taxi services 10000 = 1 Gold	default: 0
+	FLIGHT[1][3] = 500000				-- Custom item ID as cost price				default: 0
+	FLIGHT[1][4] = 50					-- Amount of custom item to take			default: 0
+	-- end Flight 1
 
--- Flight 2
-FLIGHT[2] = {} 
-FLIGHT[2][0] = "Take me to Stranglethorn Vale."		-- NPC Gossip Menu Text
-FLIGHT[2][1] = {	
-	{0, -13838, 3098, 5}, 			-- WAYPOINT 1
-	{0, -13947, 3106, 180},			-- WAYPOINT 2
-	{0, -14099, 3111, 250},
-}
-FLIGHT[2][2] = 2500					-- Cost for taxi services 10000 = 1 Gold
-FLIGHT[2][3] = 500000				-- Custom item ID as cost price
-FLIGHT[2][4] = 50					-- Amount of custom item to take
--- end Flight 2
+	-- Flight 2 - With Condition
+	if (player:GetLevel() >= 30) then
+		FLIGHT[2] = {}
+		FLIGHT[2][0] = "Take me to Stranglethorn Vale."		-- NPC Gossip Menu Text
+	
+		if player:IsAlliance() then
+			FLIGHT[2][1] = {
+				{0, -13838, 3098, 5},
+				{0, -13800, 2913, 60},			-- WAYPOINT 1
+				{0, -11758, 107, 134},			-- WAYPOINT 2
+				{0, -11614, -41, 12},
+			}
+		else -- horde
+			FLIGHT[2][1] = {
+				{0, -13838, 3098, 5},
+				{0, -13800, 2913, 60},			-- WAYPOINT 2
+				{0, -12438, 296, 25},			-- WAYPOINT 2
+				{0, -12391, 196, 4},
+			}
+		end
+		FLIGHT[2][2] = 2500					-- Cost for taxi services 10000 = 1 Gold
+		FLIGHT[2][3] = 500000				-- Custom item ID as cost price
+		FLIGHT[2][4] = 50					-- Amount of custom item to take
+	end
+	-- end Flight 2
+	
+end
+
+
 
 -- Functions
 local function startFlight(player, fly)
@@ -55,6 +74,7 @@ local function startFlight(player, fly)
 end
 
 function On_Gossip(event, player, unit)
+	flightConfig(player)
 	local i = 1
 	
 	-- Add to gossip menu
@@ -67,6 +87,7 @@ function On_Gossip(event, player, unit)
 end
 
 function On_Select(event, player, unit, sender, intid, code)
+	flightConfig(player)
 	startFlight(player, FLIGHT[intid])
 	player:GossipComplete()
 end
