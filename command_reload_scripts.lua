4539ae4617e4102bd4cd228ca9e5092ccb56a035
RStijn <raeymaekers.stijn@gmail.com>
2015-04-07 05:31:39 +0200

    Added security to Reload Scripts

--- command_reload_scripts.lua
+++ command_reload_scripts.lua
@@ -1,12 +1,16 @@
 -- Name: 	Reload Scripts
 -- Details:	Reloads lua scripts for faster development.
--- Usage:	Type "reload scripts" in the mangosd console.		
+-- Usage:	Type "reload scripts" in the mangosd console or in-game.		
 -- Website: https://github.com/RStijn
 
 -- Functions
 function reloadElunaEngine(event, player, command)
   if command == "reload scripts" or command == "reloadscripts" then 
-	ReloadEluna()
+	if player == nil or player:IsGM() then -- console or gm
+		ReloadEluna()
+	else
+		player:SendBroadcastMessage("Turn <GM> mode on for access to this command")
+	end
   end
 end
 
